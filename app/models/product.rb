29bd40f0bc9ed432b296abdb0b66195fd2d3fd64
PatreewIgor <patreewigor@yandex.ru>
2015-05-12 09:07:49 +0300

    add papperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ActiveRecord::Base
 	has_many :line_items
 	before_destroy :ensure_not_referenced_by_any_line_item
 
-
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 
 	def self.get_massive_title_product
@@ -13,14 +14,6 @@ class Product < ActiveRecord::Base
 		end
 		return massive_products_for_title_page.uniq
 	end
-
-
-
-
-
-
-
-
 
 
 	private
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508092927) do
+ActiveRecord::Schema.define(version: 20150512053626) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -43,10 +43,14 @@ ActiveRecord::Schema.define(version: 20150508092927) do
     t.text     "title"
     t.text     "description"
     t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "image"
     t.text     "types"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
